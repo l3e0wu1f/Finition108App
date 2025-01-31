b5c2cec60a202c337f9dad509bff20954fbdf61e
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-01-31 13:48:10 -0500

    Update index.js
    S3 bucket configuration for serving imagery via DigitalOcean storage CDN

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,36 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const portfolioRoutes = require('./routes/portfolioRoutes');
 const contactFormRoutes = require('./routes/contactFormRoutes');
+const AWS = require('aws-sdk'); // Adding support for s3 bucket
 
 app.use(cors());
 
 app.use(express.json());  // Pour parser le JSON
 
-// Serve images from the 'uploads' folder
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+// Existing code to serve images from the local 'uploads' directory
+// app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+
+// Configure AWS SDK
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'tor-1'
+});
+
+// Serve images from S3, including sub-directories
+app.get('/uploads/*', (req, res) => {
+  const filePath = req.params[0]; // Capture the full file path from the URL
+
+  const params = {
+    Bucket: 'imagery',
+    Key: `uploads/${filePath}`, // Use the full file path including sub-directories
+    Expires: 60 // URL expiration time in seconds
+  };
+
+  const url = s3.getSignedUrl('getObject', params);
+  res.redirect(url);
+
+
 
 
 app.get('/api', (req, res) => {
