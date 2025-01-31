7a6c6f524937d839a1c6b14e9f78d5367c3a9575
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-01-31 16:15:00 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());  // Pour parser le JSON
 // app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 
 // Configure the S3 client
-const s3 = new AWS.S3({
+const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'tor1'
