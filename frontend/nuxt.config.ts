9cc96f8ff87647780c59332fe71a6bf7463420b8
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-11 11:13:58 -0500

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    buildDir: '.output',
     nitro: {
       preset: 'node-server',
       output: '/app/.output',
