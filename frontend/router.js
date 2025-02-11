756b3c6e63ce6c15301c96c79ced1c2cd1b0ef77
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-11 17:26:07 -0500

    Update router.js

--- frontend/router.js
+++ frontend/router.js
@@ -4,37 +4,6 @@ export default defineNuxtPlugin(nuxtApp => {
   // Access the Nuxt router
   const router = useRouter();
 
-  // Add custom routes
-  router.addRoute({
-    path: '/AboutUs',
-    name: 'aboutus',
-    component: () => import('@/pages/AboutUs.vue'),
-  });
-
-  router.addRoute({
-    path: '/Services',
-    name: 'services',
-    component: () => import('@/pages/Services.vue'),
-  });
-
-  router.addRoute({
-    path: '/Process',
-    name: 'process',
-    component: () => import('@/pages/Process.vue'),
-  });
-
-  router.addRoute({
-    path: '/Contact',
-    name: 'contact',
-    component: () => import('@/pages/Contact.vue'),
-  });
-
-  router.addRoute({
-    path: '/portfolio/all',
-    name: 'portfolio',
-    component: () => import('@/pages/portfolio/all.vue'),
-  });
-
   // Optional: You can set a fallback route for undefined paths
   router.addRoute({
     path: '*',
