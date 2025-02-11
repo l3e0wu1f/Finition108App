9471a7701d10198d0cace27db6a87205ff38467d
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-11 17:08:03 -0500

    Update all.vue

--- frontend/pages/portfolio/all.vue
+++ frontend/pages/portfolio/all.vue
@@ -50,7 +50,7 @@ const portfolio = ref({ descriptionfr: '', descriptionen: '' }) // Initialize th
 
 onMounted(async () => {
   try {
-    const response = await fetch(`${process.env.API_BASE_URL}/api/portfolio/all`) // Use environment variable for the base URL
+    const response = await fetch(`/api/portfolio/all`) // Use environment variable for the base URL
     const data = await response.json()
     console.log(data);
     portfolios.value = data.portfolios
