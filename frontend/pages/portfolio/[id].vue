0d77f683a353789ba408b26b43760eeb49a6f39f
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-12 18:01:51 -0500

    Update [id].vue

--- frontend/pages/portfolio/[id].vue
+++ frontend/pages/portfolio/[id].vue
@@ -51,11 +51,13 @@ onMounted(async () => {
     const response = await fetch(`/api/portfolio/${id}`) // Adjust the endpoint as needed
     const data = await response.json()
     portfolio.value = data.portfolio
-    images.value = data.images.map(image => `https://imagery.tor1.cdn.digitaloceanspaces.com/uploads/${image}`);
+    images.value = data.images  // Use the image URLs directly
+    // images.value = data.images.map(image => `https://imagery.tor1.cdn.digitaloceanspaces.com/uploads/${image}`);
   } catch (error) {
     console.error('Error fetching portfolios:', error)
   }
 })
+
 
 const pageTitle = 'Projet ' + route.params.id
 const pageDescription = "Parcourez notre portfolio pour voir des exemples de notre savoir-faire et des divers projets que nous avons réalisés. Laissez-vous inspirer par les transformations que nous avons accompli dans des maisons et des entreprises."
