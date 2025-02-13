019c12703235e736459ab10c82a11cb7b1315a6e
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-13 11:38:26 -0500

    Update AboutUs.vue

--- frontend/pages/AboutUs.vue
+++ frontend/pages/AboutUs.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="min-h-[calc(100vh-13rem)]">
     <div
-      class="bg-fixed bg-center bg-cover h-full pt-10 md:pt-28 relative pb-10 md:pb-16"
-      style="background-image: url('https://imagery.tor1.cdn.digitaloceanspaces.com/frontend/side_scaffolding.jpg');"
+      class="bg-fixed bg-center bg-contain object-contain sm:bg-cover sm:object-cover pt-10 md:pt-28 relative pb-10 md:pb-16"
+      style="background-image: url('https://imagery.tor1.cdn.digitaloceanspaces.com/frontend/side_scaffolding.jpg'); background-repeat: no-repeat;"
     >
       <div class="z-10">
         <h1 
