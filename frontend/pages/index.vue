572a7e1698dc7298c8939f62f4b0a86ca646b7e7
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-13 16:15:16 -0500

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -18,7 +18,7 @@
           </a>
         </div>
         <!-- Above 450px wide -->
-        <div class="hidden xs:flex flex-col justify-center p-4 lg:p-14 z-5 xs:absolute lg:h-[25rem] md:bottom-0 right-0">
+        <div class="hidden xs:flex flex-col justify-center p-4 lg:p-14 z-5 xs:absolute lg:h-[25rem] sm:bottom-0 right-0">
           <h1
             class="text-lg xs:text-2xl md:text-3xl lg:text-5xl text-secondary font-medium text-right p-1"
             style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);"
@@ -41,27 +41,6 @@
         </div>
       </div>
 
-    </div>
-    <div class="flex flex-col justify-center p-4 lg:p-14 z-5 absolute lg:h-[25rem] md:bottom-0 right-0">
-      <h1
-        class="text-lg sm:text-2xl md:text-3xl lg:text-5xl text-secondary font-medium text-right p-1"
-        style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);"
-      >
-        {{ locale === 'fr' ? "Finition 108 : Maîtriser l'art de la construction" :'Finition 108: Mastering the art of' }}
-      </h1>
-      <h1
-        class="text-lg sm:text-2xl md:text-3xl lg:text-5xl text-secondary font-medium text-right p-1"
-        style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);"
-      >
-        {{ locale === 'fr' ? "depuis plus de 20 ans." :'construction for over 20 years.' }}
-      </h1>
-
-      <a
-        href="/Contact"
-        class="w-60 lg:w-80 h-16 flex justify-center items-center rounded-2xl text-lg font-light text-center bg-primarybis/60 hover:primarybisary/80 text-secondary self-end mt-10"
-      >
-        {{ locale === 'fr' ? 'Contactez-nous' : 'Contact us' }}
-      </a>
     </div>
     <div class="bg-secondary z-10">
       <div class="lg:h-[calc(100vw/2.5)] flex flex-col lg:flex-row items-center relative overflow-hidden justify-between">
