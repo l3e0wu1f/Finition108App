3b76a1e43b8612772e9c20c66bc0537a24749dfb
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-13 17:24:03 -0500

    Update Services.vue

--- frontend/pages/Services.vue
+++ frontend/pages/Services.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-screen min-h-screen">
     <div
-      class="sm:bg-fixed bg-center bg-contain object-contain sm:bg-cover sm:object-cover h-full pt-10 md:pt-28 relative"
+      class="xs:bg-fixed xs:bg-center bg-contain object-contain xs:bg-cover xs:object-cover h-full pt-10 md:pt-28 relative"
       style="background-image: url('https://imagery.tor1.cdn.digitaloceanspaces.com/frontend/paint_brush.jpg'); background-repeat: no-repeat;"
     >
       <div class="z-10">
