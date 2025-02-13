37f4297ec9574224c72ac51c37411c793e4467e8
Josh Lewis <37599831+l3e0wu1f@users.noreply.github.com>
2025-02-13 16:32:09 -0500

    Update iconify.js

--- frontend/public/iconify.js
+++ frontend/public/iconify.js
@@ -1,17 +1,1 @@
-import Vue from 'vue';
-import { Icon } from '@iconify/vue';
-import chevronLeft from '@iconify/icons-heroicons/arrow-left-20-solid';
-import chevronRight from '@iconify/icons-heroicons/arrow-right-20-solid';
-
-export default {
-  components: {
-    Icon
-  },
-  data() {
-    return {
-      chevronLeftIcon: chevronLeft,
-      chevronRightIcon: chevronRight
-    };
-  }
-};
 
